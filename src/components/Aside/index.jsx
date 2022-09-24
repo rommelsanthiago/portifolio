2ed90fd91feba9268cbd3a27514063ad924ca326
rommelsanthiago <rommel.santhiago@gmail.com>
2022-09-24 20:56:11 -0300

    Styling aside

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import { House, User, Code, Briefcase, Chats } from "phosphor-react"
+import { 
+  House, 
+  User, 
+  Code, 
+  Briefcase, 
+  Chats,
+  LinkedinLogo,
+  GithubLogo,
+  TwitterLogo,
+  YoutubeLogo,
+  InstagramLogo,
+  StackOverflowLogo,
+  DiscordLogo
+} from "phosphor-react"
 
 import * as S from "./styles"
+import Avatar from "../../assets/images/perfil.png";
 
 const Aside = ({ setPage }) => {
   return (
     <S.Aside>
+      <S.Img src={Avatar} alt="Rommel Santhiago" />
       <nav>
         <S.Ul>
           <li>
-            <S.Button onClick={() => setPage("home")}>
+            <S.Button 
+              onClick={() => setPage("home")}
+            >
               <House weight="fill" />
               Home
             </S.Button>
@@ -40,6 +57,29 @@ const Aside = ({ setPage }) => {
           </li>
         </S.Ul>
       </nav>
+      <S.Social>
+        <a href="https://www.linkedin.com/in/rommelsanthiago" target="_blank" >
+          <LinkedinLogo weight="fill" />
+        </a>
+        <a href="https://github.com/rommelsanthiago" target="_blank" >
+          <GithubLogo weight="fill" />
+        </a>
+        <a href="https://twitter.com/RommelSanthiago" target="_blank" >
+          <TwitterLogo weight="fill" />
+        </a>
+        <a href="https://www.youtube.com/channel/UC3IpAXprcUJRaCn7G7Hh9YQ" target="_blank" >
+          <YoutubeLogo weight="fill" />
+        </a>
+        <a href="https://www.instagram.com/rommelsanthiago/" target="_blank" >
+          <InstagramLogo weight="fill" />
+        </a>
+        <a href="https://pt.stackoverflow.com/users/271415/rommel-santhiago" target="_blank" >
+          <StackOverflowLogo weight="fill" />
+        </a>
+        <a href="https://discord.com/users/724842559779766312" target="_blank" >
+          <DiscordLogo weight="fill" />
+        </a>
+      </S.Social>
     </S.Aside>
   );
 };
--- src/components/Aside/styles.js
+++ src/components/Aside/styles.js
@@ -3,38 +3,69 @@ import styled from "styled-components";
 export const Aside = styled.aside`
     width: 25%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     border-right: 1px solid;
+    padding-top: 1rem;
     position: fixed;
     left: 0;
     top: 0;
 `
 
+export const Img = styled.img`
+    max-width: 70%;
+    filter: drop-shadow(1px 1px 2px green)
+        drop-shadow(-1px -1px 2px green)
+`
+
 export const Ul = styled.ul`
-    height: 10rem;
+    height: 15rem;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     list-style-type: none;
+    padding: 0;
 `
 
-export const Button = styled.button`
-    width: 70%;
+export const Button = styled("button")`
+    width: 100%;
     display: flex;
     align-items: center;
     background: none;
     border: none;
     cursor: pointer;
-    font-size: 1.2rem;
+    font-size: 1.5rem;
+    font-weight: 600;
     border-bottom: 1px solid;
     margin-top: 1rem;
-    padding: 0 0 0.5rem 1.2rem;
+    transition: all 0.3s ease;
 
-    &: hover {
+    &: focus, &: hover {
         border-bottom: 1px solid green;
         color: green;
+        transform: scale(1.1);
     }
 
     svg {
         margin-right: 0.5rem;
+    }
+`
+
+export const Social = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: space-around;
+    padding-top: 5rem;
+
+    svg {
+        color: black;
+        font-size: 1.2rem;
+        transition: all 0.3s ease;
+
+        &: hover {
+            color: green;
+            transform: scale(1.3);
+        }
     }
 `
