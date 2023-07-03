73d56164549ae2b1c5a1684101cc01f0387b5f29
rommelsanthiago <rommel.santhiago@gmail.com>
2023-07-03 18:37:52 -0300

    WEB ANALYTICS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from '@vercel/analytics';
+
 import App from "./App";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
